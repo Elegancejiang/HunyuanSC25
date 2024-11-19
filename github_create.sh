f1d6527941bcce098d70e863c87d61fe8b08709e
jiangdie <liyongjiang2001@163.com>
2024-11-19 14:18:12 +0800

    xx.xx.xx update

--- github_create.sh
+++ github_create.sh
@@ -2,6 +2,7 @@ echo "# HunyuanGraph_SC24" >> README.md
 git init
 # 添加所有文件到暂存区
 git add .
+# 描述更改内容
 git commit -m "first commit"
 git branch -M main
 git remote add origin git@github.com:Elegancejiang/HunyuanGraph_SC24.git
---
+++ github_update.sh
@@ -0,0 +1,11 @@
+# 查看哪些文件被修改、新增或删除
+git status
+# 添加所有文件到暂存区
+git add .
+# 描述更改内容
+git commit -m "xx.xx.xx update"
+# 上传
+git push
+
+# 下载
+# git pull origin main
