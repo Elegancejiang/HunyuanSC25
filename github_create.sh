3de0ad1d9aaee1e70c1fd191d342fbb8b548c56d
jiangdie <liyongjiang2001@163.com>
2024-11-19 14:29:23 +0800

    first commit

--- github_create.sh
+++ github_create.sh
@@ -5,5 +5,7 @@ git add .
 # 描述更改内容
 git commit -m "first commit"
 git branch -M main
+# 移除旧的远程仓库
+git remote remove origin
 git remote add origin git@github.com:Elegancejiang/HunyuanGraph_SC25_0.0.0.git
 git push -u origin main
