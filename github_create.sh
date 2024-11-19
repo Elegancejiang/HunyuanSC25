1b60648fc5489d1d7c5dfb5740995643d18acd2f
jiangdie <liyongjiang2001@163.com>
2024-11-19 14:27:32 +0800

    first commit

--- github_create.sh
+++ github_create.sh
@@ -1,9 +1,9 @@
-echo "# HunyuanGraph_SC24" >> README.md
+echo "# HunyuanGraph_SC25_0.0.0" > README.md
 git init
 # 添加所有文件到暂存区
 git add .
 # 描述更改内容
 git commit -m "first commit"
 git branch -M main
-git remote add origin git@github.com:Elegancejiang/HunyuanGraph_SC24.git
+git remote add origin git@github.com:Elegancejiang/HunyuanGraph_SC25_0.0.0.git
 git push -u origin main
