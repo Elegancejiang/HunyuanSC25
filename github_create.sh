a2ef300ee0b331563bbebdbd92e4a083fa1b1387
unknown <liyongjiang2001@163.com>
2025-04-25 22:02:02 +0800

    first commit

--- github_create.sh
+++ github_create.sh
@@ -7,7 +7,7 @@ git commit -m "first commit"
 git branch -M main
 # 移除旧的远程仓库
 git remote remove origin
-git remote add origin git@github.com:Elegancejiang/HunyuanGraph_SC25_0.0.0.git
+git remote add origin git@github.com:Elegancejiang/HunyuanSC25.git
 # 更改远程仓库
-git remote set-url origin git@github.com:Elegancejiang/HunyuanSC25.git
+# git remote set-url origin git@github.com:Elegancejiang/HunyuanSC25.git
 git push -u origin main
